7533994469959529332f79c5187b59c423dbf17b
zudikas-zveris <zudikas-zveris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-09 07:29:49 +0000

    v1.3.1 FIX
    git-svn-id: https://plugins.svn.wordpress.org/zz-image-slider/trunk@1241259 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zz-image-slider.php
+++ zz-image-slider.php
@@ -5,7 +5,7 @@
  * Description: Responsive images slider, bassed on JSSOR Slider.
  * Author:      zudikas zveris
  * Author URI:  http://zzapps.net
- * Version:     1.3.0
+ * Version:     1.3.1
  * Text Domain: zz-images-slider
  * Domain Path: /languages/
  * License:     GPLv2 or later
